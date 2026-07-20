83fa66a9a4de8ada864777a4dadc10f65feda8f9
Nathan Tredgett <nathantredgett@gmail.com>
2026-07-20 11:38:03 +0100

    feat: added channel flag... hopefully

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ channels_url = "https://iptv-org.github.io/api/channels.json"
 feeds_url = "https://iptv-org.github.io/api/feeds.json"
 streams_url = "https://iptv-org.github.io/api/streams.json"
 logos_url = "https://iptv-org.github.io/api/logos.json"
+countries_url = "https://iptv-org.github.io/api/countries.json"
 
 #  channels.network AS channel_network,
 # streams.title AS stream_title,
@@ -15,11 +16,12 @@ def write_m3u(df: DataFrame):
     with open("custom-playlist.m3u", "w", encoding="utf-8") as f:
         f.write("#EXTM3U\n")
         for _, row in df.iterrows():
+            name = f"{row['channel_name']} {row['country_flag']}"
             f.write(
-                f'#EXTINF:-1 tvg-id="{row["channel_id"]}" tvg-name="{row["channel_name"]}" '
+                f'#EXTINF:-1 tvg-id="{row["channel_id"]}" tvg-name="{name}" '
                 f'group-title="{row["channel_categories"]}" '
                 f'tvg-logo="{row["logo_url"]}",'
-                f'{row["channel_name"]}\n'
+                f'{name}\n'
             )
             f.write(f'{row["stream_url"]}\n')
 
@@ -34,7 +36,8 @@ df = duckdb.sql(
         channels.categories AS channel_categories,
         feeds.is_main AS feed_is_main,
         streams.url AS stream_url,
-        streams.quality AS stream_quality
+        streams.quality AS stream_quality,
+        countries.flag AS country_flag,
     FROM '{channels_url}' AS channels
     JOIN '{feeds_url}' AS feeds
         ON channels.id = feeds.channel
@@ -42,6 +45,8 @@ df = duckdb.sql(
         ON channels.name = streams.title
     JOIN '{logos_url}' AS logos
         ON channels.id = logos.channel
+    JOIN '{countries_url}' AS countries
+        ON channels.country = countries.code
     WHERE channels.closed IS NULL
       AND channels.network NOT LIKE '%Pluto%'
       AND (
@@ -62,7 +67,7 @@ df = duckdb.sql(
 
 # OR streams.quality LIKE '%720%'
 df = df.drop_duplicates("stream_url")
-
+print(df)
 write_m3u(df)
 
 print(f"Playlist generated with {len(df)} channels.")
